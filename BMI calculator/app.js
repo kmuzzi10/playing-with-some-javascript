b0b0296645ead7aaa91dfc9e83118ce873d26b69
kmuzzi10 <kmuzammil12546@gmail.com>
2023-06-16 02:28:19 +0500

    bmi calculator

--- BMI calculator/app.js
+++ BMI calculator/app.js
@@ -1,3 +1,5 @@
+//this is bmi calculator in which you calculate your bmi by giving function height and weight
+
 function bmiCalculator(weight,height){
     var bmi = weight / (height*height)
     return Math.round(bmi)
