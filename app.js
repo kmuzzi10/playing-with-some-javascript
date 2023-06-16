c1bc6fb0ef7fa6f8323ded961eacac5214bf5c3d
kmuzzi10 <kmuzammil12546@gmail.com>
2023-06-17 02:52:56 +0500

    playing

--- app.js
+++ app.js
@@ -2,20 +2,7 @@
 var x = 5;
 var y = 5;
 //increment
-x++ //value of x is 6 now 
-x += y //it add y value into x now x value is 10 we can also use -= to subtract the value from x and *= and /=
-
-
-
-//bottels
-
-
-
-
-
-
-
-
-
+x++; //value of x is 6 now 
+x += y ;//it add y value into x now x value is 10 we can also use -= to subtract the value from x and *= and /=
 
 
---
+++ love calculator using random number generator/app.js
@@ -0,0 +1,5 @@
+var name1 = prompt('what is your name');
+var name2 = prompt('what is their name');
+var loveCal = Math.random()*100;
+loveCal = Math.floor(loveCal);
+alert('you '+name1+' and '+name2+' have '+loveCal+'% love')
