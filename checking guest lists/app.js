c25172c3cc9262dcfc2d88845eaa950dc636c2d7
kmuzzi10 <kmuzammil12546@gmail.com>
2023-06-19 02:30:49 +0500

    practice

--- checking guest lists/app.js
+++ checking guest lists/app.js
@@ -1,10 +1,3 @@
-//increment and decrement
-var x = 5;
-var y = 5;
-//increment
-x++; //value of x is 6 now 
-x += y;//it add y value into x now x value is 10 we can also use -= to subtract the value from x and *= and /=
-
 var guestList = ['mudassir','maham','maheen','muzammil','shayan','gaffar']
 var guestName = prompt('what is your name')
 if(guestList.includes(guestName)){
