fb7249d76b61e40b88ef98f67e6dc5f1b91190dd
kmuzzi10 <kmuzammil12546@gmail.com>
2023-06-19 04:24:52 +0500

    practice

--- fizzBuzz challange/app.js
+++ fizzBuzz challange/app.js
@@ -1,10 +1,3 @@
-//increment and decrement
-var x = 5;
-var y = 5;
-//increment
-x++; //value of x is 6 now 
-x += y;//it add y value into x now x value is 10 we can also use -= to subtract the value from x and *= and /=
-
 var say = [];
 var count=1;
 var fizzBuzz = ()=>{
@@ -40,7 +33,3 @@ fizzBuzz()
 fizzBuzz()
 fizzBuzz()
 fizzBuzz()
-
-
-
-
