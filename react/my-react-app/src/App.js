a053db078ade20f01ae070e287bcd83eb1bcdffa
kmuzzi10 <kmuzammil12546@gmail.com>
2023-10-01 03:46:22 +0500

    react

---
+++ react/my-react-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" >Lists</h1>
+    <ul>
+      <li>ok</li>
+      <li>acha</li>
+      <li>thk</li>
+    </ul>
+  </div>,
+  document.getElementById('root')
+)
+
+
+export default App;
