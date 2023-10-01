998a20bc45377aef36177c14fb2607bda51aa9b9
kmuzzi10 <kmuzammil12546@gmail.com>
2023-10-02 04:21:22 +0500

    practice

--- react/my-react-app/src/App.js
+++ react/my-react-app/src/App.js
@@ -4,12 +4,12 @@ import App from './App';
 
 ReactDOM.render(
   <div>
-    <h1 className="heading" >Lists</h1>
-    <ul>
-      <li>ok</li>
-      <li>acha</li>
-      <li>thk</li>
-    </ul>
+    <h1 className="heading">Lists</h1>
+    <div>
+    <img  className="img-class" src="https://assets.bonappetit.com/photos/643f14b5259b5fd1329ed8f5/1:1/w_3987,h_3987,c_limit/Noodle_Type_Ramen_Finished_Still_Select_COMP.jpg" alt=""></img>
+    <img className="img-class" src="https://thewoksoflife.com/wp-content/uploads/2020/04/homemade-chinese-egg-noodles-19-e1609271249794.jpg" alt=""></img>
+    <img className="img-class" src="https://static.toiimg.com/thumb/52467119.cms?width=1200&height=900" alt=""></img>
+    </div>
   </div>,
   document.getElementById('root')
 )
