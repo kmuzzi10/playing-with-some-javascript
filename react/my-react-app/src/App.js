75acb45c9aa7342d90c097ba2461d6c645ffa529
kmuzzi10 <kmuzammil12546@gmail.com>
2023-10-03 02:21:50 +0500

    practice

--- react/my-react-app/src/App.js
+++ react/my-react-app/src/App.js
@@ -2,17 +2,64 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
 
+
+// const customCss = {
+//   color:"red",
+//   fontSize:"4rem"
+// }
+
+// customCss.color = "blue"        
+
+
+
+
+// ReactDOM.render(
+//   <div>
+//     <h1 style={customCss}>Lists</h1>        {/* by changing css through object */}
+//     <div>
+//       {/* inline css  */}
+//     <img style={{width:"100px",height:"100px"}}   src="https://assets.bonappetit.com/photos/643f14b5259b5fd1329ed8f5/1:1/w_3987,h_3987,c_limit/Noodle_Type_Ramen_Finished_Still_Select_COMP.jpg" alt=""></img>
+//     <img style={{width:"100px",height:"100px"}} src="https://thewoksoflife.com/wp-content/uploads/2020/04/homemade-chinese-egg-noodles-19-e1609271249794.jpg" alt=""></img>
+//     <img style={{width:"100px",height:"100px"}} src="https://static.toiimg.com/thumb/52467119.cms?width=1200&height=900" alt=""></img>
+//     </div>
+//   </div>,
+//   document.getElementById('root')
+// )
+
+let textColor = "";
+let greeting = ""
+
+
+const date = new Date();
+const hour = date.getHours();
+if(hour>=1 && hour<6){
+  textColor = "black";
+  greeting = "good night"
+}
+else if(hour>=6 && hour<12){
+textColor = "red";
+greeting = "good morning"
+}else if(hour>=12 && hour<18 ){
+textColor = "green"
+greeting = "good afternoon"
+}else{
+  textColor = "blue"
+  greeting = "good evening"
+}
+
+const colorCss = {
+  color : textColor
+}
 ReactDOM.render(
   <div>
-    <h1 className="heading">Lists</h1>
-    <div>
-    <img  className="img-class" src="https://assets.bonappetit.com/photos/643f14b5259b5fd1329ed8f5/1:1/w_3987,h_3987,c_limit/Noodle_Type_Ramen_Finished_Still_Select_COMP.jpg" alt=""></img>
-    <img className="img-class" src="https://thewoksoflife.com/wp-content/uploads/2020/04/homemade-chinese-egg-noodles-19-e1609271249794.jpg" alt=""></img>
-    <img className="img-class" src="https://static.toiimg.com/thumb/52467119.cms?width=1200&height=900" alt=""></img>
-    </div>
+    <h1 style={colorCss}>{greeting}</h1>
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 )
+
+
+
+
 
 
 export default App;
---
+++ react/my-react-app/src/ok.js
@@ -0,0 +1,3 @@
+const date = new Date();
+const time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+console.log(time)
