7f882c327c1da667e226641dd221205bcdb05b8a
kmuzzi10 <kmuzammil12546@gmail.com>
2023-10-07 01:52:02 +0500

    practice

--- react/my-react-app/src/ok.js
+++
@@ -1,3 +0,0 @@
-const date = new Date();
-const time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-console.log(time)
---
+++ react/trial/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(<h1>Hello World</h1>,document.getElementById('root'));
+
